dd3946fbd738ab7ad1396da45c8b54273114d6a6
cucchi10 <cucchi.sm@hotmail.com>
2022-05-28 14:22:14 -0300

    :D

--- redes convolusionales/Código Redes Convolusionales.py
+++ redes convolusionales/Código Redes Convolusionales.py
@@ -9,7 +9,8 @@ labels=[] #labels es un lista con un valor para cada imagen 0 para las de la pri
 
 for index, i in enumerate(categorias):
     for k in range(1,10): #son 10 imagenes por carpeta
-        img=cv2.imread(f'{i}/{k}.jpg',0) #almaceno cada imagen en escala de grises
+        url=(f'redes convolusionales/{i}/{k}.jpg')
+        img=cv2.imread(url,0) #almaceno cada imagen en escala de grises
         img=cv2.resize(img,(200,200)) #redimensiono cada imagen a 200 x 200 por la cantidad de neuronas de entrada
         img=np.asarray(img)#convierto la imagene en una lista de valor para cada px entre 0 y 255
         imagenes.append(img) #agrego cada lista a la lista imagenes
@@ -58,7 +59,7 @@ imagenes=np.array(imagenes)
 labels=np.array(labels)
 model.fit(imagenes , labels , epochs=20)
 
-test=cv2.imread("test.jpg",0) # imporoto la prieba en gris
+test=cv2.imread("redes convolusionales/test.jpg",0) # imporoto la prieba en gris
 test=cv2.resize(test,(200,200)) #la redimensiono
 test=np.asarray(test) #lo convierto en arreglo de px
 test=np.array([test]) #econvierto a un arreglo numpy  es un alista dentro de otra lista porque ese es el formato que tiene el set de imagenes)
